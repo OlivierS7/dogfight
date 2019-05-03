8f0438e917d61734a315e51b98d3fd1e85cf7320
OlivierS7 <44491595+OlivierS7@users.noreply.github.com>
2019-05-03 10:36:27 +0200

    Phase 3

---
+++ src/jpu2016/dogfight/controller/DogFightController.java
@@ -0,0 +1,32 @@
+package jpu2016.dogfight.controller;
+
+import jpu2016.dogfight.model.IDogfightModel;
+import jpu2016.dogfight.view.IViewSystem;
+
+public class DogFightController {
+	private static int TIME_SLEEP = 30;
+	
+	public DogFightController(IDogfightModel dogfightModel) {
+		
+	}
+	
+	public void orderPerform(UserOrder userOrder) {
+		
+	}
+	
+	public void play() {
+		
+	}
+	
+	public void setViewSystem(IViewSystem viewSystem) {
+		
+	}
+	
+	private void lauchMissile(int player) {
+		
+	}
+	
+	private void gameLoop() {
+		
+	}
+}
