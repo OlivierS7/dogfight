32411648e399a542d6311183aec249e470448216
sqartox <sqartox@users.noreply.github.com>
2019-05-03 10:31:20 +0200

    Phase 2

---
+++ src/jpu2016/dofight/model/Direction.java
@@ -0,0 +1,5 @@
+package jpu2016.dofight.model;
+
+public enum Direction {
+	UP, DOWN, LEFT, RIGHT;
+}
---
+++ src/jpu2016/dofight/model/IDogfightModel.java
@@ -0,0 +1,18 @@
+package jpu2016.dofight.model;
+
+import java.util.ArrayList;
+
+public interface IDogfightModel {
+	public IArea getArea();
+
+	public void buildArea(Dimension dimension);
+
+	public void addMobile(IMobile Mobile);
+
+	public ArrayList<IMobile> getMobiles();
+
+	public IMobile getMobileByPlayer(int player);
+
+	public void setMobilesHavesMoved();
+
+}
