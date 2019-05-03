54e97b0a28ee303e77af941f3b8ef3435de6f60d
sqartox <sqartox@users.noreply.github.com>
2019-05-03 10:14:46 +0200

    Finished

---
+++ src/jpu2016/dogfight/model/IDogFightModel.java
@@ -0,0 +1,19 @@
+package jpu2016.dogfight.model;
+
+import java.util.ArrayList;
+
+public interface IDogFightModel {
+	public IArea getArea();
+
+	public void buildArea();
+
+	public void addMobile();
+
+	public void removeMobile();
+
+	public ArrayList<IMobile> getMobiles();
+
+	public IMobile getMobileByPlayer(int player);
+
+	public void setMobilesHavesMoved();
+}
